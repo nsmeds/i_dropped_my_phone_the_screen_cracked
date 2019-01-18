bedbdddd42b9cdcac6f242b4ed8dfa8b2bfd9e79
niilo smeds <niilo.smeds@nike.com>
2019-01-18 08:47:27 -0800

    add failing test, colorize test output

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,19 @@
+require('../dist/cracked.js');
 const test = require('tape');
-const cracked = require('../dist/cracked.js');
 
-console.log('cracked is', cracked);
-
-test('find.js loads without error', function (t) {
-  t.doesNotThrow(window.cracked.filter);
-  t.ok(window.__);
+test('window.cracked loads without error', function(t) {
   t.ok(window.cracked);
+  t.doesNotThrow(window.cracked);
+  t.end();
+});
+
+test('window.cracked is aliased to window.__', function(t) {
+  t.deepEqual(window.cracked, window.__);
+  t.end();
+});
+
+test('find.js loads without error', function(t) {
+  t.doesNotThrow(window.cracked.find('something')); // this is throwing
   t.ok(window.cracked.find('hi'));
-  t.doesNotThrow(window.cracked.reset);
   t.end();
 });
